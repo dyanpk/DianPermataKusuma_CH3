17be2356ae57194e8b630c0286d8021b29baf444
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-09-21 12:34:51 +0700

    Initial commit

---
+++ app/src/main/java/com/hungry/rv_menu_fragment_detail/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hungry.rv_menu_fragment_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
